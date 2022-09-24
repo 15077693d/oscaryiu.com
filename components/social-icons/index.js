8a542a82f7197327717b8b7bda89ee4df46554d6
15077693d <oscaryiu.lapsang@gmail.com>
2022-09-25 00:48:12 +0800

    remove console.log and add md back

--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -25,7 +25,6 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     return null
 
   const SocialSvg = components[kind]
-  console.log(kind, href)
   return (
     <a
       className="text-sm text-gray-500 transition hover:text-gray-600"
