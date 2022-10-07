92f4e75455e8cf5e7bdc20d6512628e2359f5074
15077693d <oscaryiu.lapsang@gmail.com>
2022-10-08 00:05:04 +0800

    update projects page

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,35 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Platypus Finance dapp',
+    description:
+      "Platypus finance is a whole new kind of automated market maker (AMM) for cryptocurrency stable swaps. I'm in a frontend team and am responsible to data fetching from blockchain and wallet integration.",
+    imgSrc: '/static/images/projects/platyus-finance.png',
+    href: 'https://app.platypus.finance/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Xentropy',
+    description:
+      'Xentropy provides a standalone AI Model-as-a-Service. I was in the frontend team and work on the landing page and AI software web interface with React.js and Firebase.',
+    imgSrc: '/static/images/projects/xentropy.png',
+    href: 'https://xentropy.co/',
+  },
+  {
+    title: 'Frontend Mentor with Tailwind CSS',
+    description: 'It records my Frontend Mentor practice journey.',
+    imgSrc: '/static/images/projects/frontend-mentor-with-tailwind-css.png',
+    href: 'https://frontendmentor.oscaryiu.com/',
+  },
+  {
+    title: 'Tailwind CSS Helper',
+    description: 'A Figma plugin to transform theme values(etc. color, text) to json format.',
+    imgSrc: '/static/images/projects/tailwind-css-helper.png',
+    href: 'https://github.com/15077693d/Tailwind-CSS-Helper',
+  },
+  {
+    title: 'Bodystation',
+    description: 'Game controller base on human movement built with Python.',
+    imgSrc: '/static/images/projects/bodystation.jpeg',
+    href: 'https://github.com/15077693d/BodyStation',
   },
 ]
 
